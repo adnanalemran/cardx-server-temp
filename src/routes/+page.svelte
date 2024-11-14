7e239246639740780d2b8bca11976e3746f6b321
adnanalemran <adnanalemranontor@gmail.com>
2024-11-15 01:13:43 +0600

    Add Tailwind CSS integration and implement image gallery in main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,57 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+  import { Gallery } from "flowbite-svelte";
+  const images = [
+    {
+      alt: "erbology",
+      src: "https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg",
+    },
+    {
+      alt: "shoes",
+      src: "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg",
+    },
+    {
+      alt: "small bag",
+      src: "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg",
+    },
+    {
+      alt: "plants",
+      src: "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg",
+    },
+    {
+      alt: "watch",
+      src: "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg",
+    },
+    {
+      alt: "shoe",
+      src: "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg",
+    },
+    {
+      alt: "cream",
+      src: "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-6.jpg",
+    },
+    {
+      alt: "small bag",
+      src: "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-7.jpg",
+    },
+    {
+      alt: "lamp",
+      src: "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-8.jpg",
+    },
+    {
+      alt: "toiletbag",
+      src: "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-9.jpg",
+    },
+    {
+      alt: "playstation",
+      src: "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-10.jpg",
+    },
+    {
+      alt: "bag",
+      src: "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-11.jpg",
+    },
+  ];
+</script>
+
+<h1 class="text-red-400">Welcome to Adnan</h1>
+
+<Gallery items={images} class="gap-4 grid-cols-2 md:grid-cols-3" />
